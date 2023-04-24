652460425005d5473c0ae8dca8480f4fd5244991
Taeuk Um <42173347+umtuk@users.noreply.github.com>
2023-04-24 18:13:25 +0900

    Create global security config

---
+++ src/main/java/org/routemaster/api/total/global/config/GlobalSecurityConfig.java
@@ -0,0 +1,23 @@
+package org.routemaster.api.total.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableReactiveMethodSecurity
+public class GlobalSecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        return http
+                .csrf().disable()
+                .httpBasic().disable()
+                .authorizeExchange(exchange -> exchange
+                        .anyExchange().permitAll()
+                )
+                .build();
+    }
+}
