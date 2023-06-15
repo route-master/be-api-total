b1353c29e934261008a38d3d72e7cc447581865b
eunbin00 <eun61n@gmail.com>
2023-06-15 18:23:57 +0900

    #2 -  Feat: Add Location based Search rest docs

--- src/main/java/org/routemaster/api/total/domain/attraction/controller/AttractionSearchRestController.java
+++ src/main/java/org/routemaster/api/total/domain/attraction/controller/AttractionSearchRestController.java
@@ -1,5 +1,11 @@
 package org.routemaster.api.total.domain.attraction.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.routemaster.api.total.domain.attraction.service.AttractionSearchService;
@@ -20,20 +26,105 @@ public class AttractionSearchRestController {
 
     private final AttractionSearchService attractionSearchService;
 
+    @Operation(
+            summary = "위치 기반 관광 정보 조회",
+            description = "위치 기반 관광 정보 파라미터 타입에 따라서 정렬(제목순, 수정일순 등) 검색목록을 조회",
+            tags = {
+                    "attraction-search"
+            }
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description= "Success Response",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = AttractionSearchVO.class)
+                    )
+            )
+    })
     @GetMapping("/location-based")
     public ResponseEntity<Mono<AttractionSearchVO>> locationBase(
-            @RequestParam(required = false) Integer numOfRows,
-            @RequestParam(required = false) Integer pageNo,
-            @RequestParam String MobileOS,
-            @RequestParam String MobileApp,
-            @RequestParam(required = false) String _type,
-            @RequestParam(required = false) String listYN,
-            @RequestParam(required = false) String arrange,
-            @RequestParam Double mapX,
-            @RequestParam Double mapY,
-            @RequestParam Integer radius,
-            @RequestParam(required = false) Integer contentTypeId,
-            @RequestParam(required = false) String modifiedTime
+            @Parameter(
+                    description = "한 페이지 결과 수",
+                    example = "10"
+            ) @RequestParam(required = false) Integer numOfRows,
+            @Parameter(
+                    description = "페이지 번호",
+                    example = "1"
+            ) @RequestParam(required = false) Integer pageNo,
+            @Parameter(
+                    description = "OS 구분",
+                    example = "ETC",
+                    schema = @Schema(
+                            allowableValues = {
+                                    "AND",
+                                    "IOS",
+                                    "WIN",
+                                    "ETC"
+                            }
+                    )
+            ) @RequestParam String MobileOS,
+            @Parameter(
+                    description = "서비스명(어플명)",
+                    example = "AppTest"
+            ) @RequestParam String MobileApp,
+            @Parameter(
+                    description = "응답메세지 형식",
+                    example = "json",
+                    schema = @Schema(
+                            allowableValues = {
+                                    "xml",
+                                    "json"
+                            }
+                    )
+            ) @RequestParam(required = false) String _type,
+            @Parameter(
+                    description = "목록 구분(Y=목록, N=개수)",
+                    example = "Y",
+                    schema = @Schema(
+                            allowableValues = {
+                                    "Y",
+                                    "N"
+                            }
+                    )
+            ) @RequestParam(required = false) String listYN,
+            @Parameter(
+                    description = "정렬 구분(A=제목순, C=수정일순, D=생성일순)",
+                    example = "A",
+                    schema = @Schema(
+                            allowableValues = {
+                                    "A",
+                                    "B",
+                                    "C",
+                                    "D"
+                            }
+                    )
+            ) @RequestParam(required = false) String arrange,
+            @Parameter(
+                    description = "GPS X좌표(WGS84 경도 좌표)",
+                    example = "126.981611"
+            ) @RequestParam Double mapX,
+            @Parameter(
+                    description = "GPS Y좌표(WGS84 위도 좌표)",
+                    example = "37.568477"
+            ) @RequestParam Double mapY,
+            @Parameter(
+                    description = "거리 반경(단위: m)",
+                    example = "20000",
+                    schema = @Schema(
+                            minimum = "1",
+                            maximum = "20000"
+                    )
+            ) @RequestParam Integer radius,
+            @Parameter(
+                    description = "관광타입(관광지, 숙박 등) ID",
+                    example = "12"
+            ) @RequestParam(required = false) Integer contentTypeId,
+            @Parameter(
+                    description = "콘텐츠 수정일",
+                    example = "2022-10-20 10:00:00.0"
+            ) @RequestParam(required = false) String modifiedTime
     ) {
         Mono<AttractionSearchVO> result = attractionSearchService.searchLocationBasedAttraction(
                 numOfRows,
--- src/main/java/org/routemaster/api/total/infra/tourapi/vo/AttractionVO.java
+++ src/main/java/org/routemaster/api/total/infra/tourapi/vo/AttractionVO.java
@@ -1,6 +1,7 @@
 package org.routemaster.api.total.infra.tourapi.vo;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
 import java.net.URI;
@@ -15,27 +16,96 @@ import java.util.Date;
 @ToString
 public class AttractionVO {
 
-    private @Getter String title;
-    private @Getter String largeCategory;
-    private @Getter String mediumCategory;
-    private @Getter String smallCategory;
-    private @Getter Double mapX;
-    private @Getter Double mapY;
-    private @Getter Integer areaCode;
-    private @Getter Integer sigunguCode;
-    private @Getter Double dist;
-    private @Getter Integer mapLevel;
-    private @Getter String address;
-    private @Getter String detailAddress;
-    private @Getter Integer contentId;
-    private @Getter Integer contentTypeId;
-    private @Getter String copyrightType;
-    private @Getter URI image;
-    private @Getter URI thumbnailImage;
-    private @Getter Date createdTime;
-    private @Getter Date modifiedTime;
-    private @Getter boolean bookTour;
-    private @Getter String tel;
+    @Schema(
+            description = "제목",
+            example = "경복궁"
+    ) private @Getter String title;
+    @Schema(
+            description = "대분류",
+            example = "A02"
+    ) private @Getter String largeCategory;
+    @Schema(
+            description = "중분류",
+            example = "A0208"
+    ) private @Getter String mediumCategory;
+    @Schema(
+            description = "소분류",
+            example = "A02080300"
+    ) private @Getter String smallCategory;
+    @Schema(
+            description = "GPS x좌표",
+            example = "126.976888"
+    ) private @Getter Double mapX;
+    @Schema(
+            description = "GPS y좌표",
+            example = "37.576264"
+    ) private @Getter Double mapY;
+    @Schema(
+            description = "지역코드",
+            example = "32",
+            allowableValues = {
+                    "1", "2", "3", "4", "5", "6", "7", "8", "31", "32"
+            }
+    ) private @Getter Integer areaCode;
+    @Schema(
+            description = "시군구코드",
+            example = "13"
+    ) private @Getter Integer sigunguCode;
+    @Schema(
+            description = "중심 좌표로부터의 거리(단위: m)",
+            example = "616.0375045908319"
+    ) private @Getter Double dist;
+    @Schema(
+            description = "Map Level 응답",
+            example = "6"
+    ) private @Getter Integer mapLevel;
+    @Schema(
+            description = "주소",
+            example = "서울특별시 종로구 사직로 161"
+    ) private @Getter String address;
+    @Schema(
+            description = "상세 주소",
+            example = "(서면) 부근"
+    ) private @Getter String detailAddress;
+    @Schema(
+            description = "콘텐츠 ID",
+            example = "126508"
+    ) private @Getter Integer contentId;
+    @Schema(
+            description = "콘텐츠 타입 ID",
+            example = "12"
+    ) private @Getter Integer contentTypeId;
+    @Schema(
+            description = "저작권 유형(Type1:제1유형(출처표시-권장) Type3:제3유형(제1유형 + 변경금지))",
+            example = "Type1",
+            allowableValues = {
+                    "Type1", "Type3"
+            }
+    ) private @Getter String copyrightType;
+    @Schema(
+            description = "대표 이미지(원본, 500*333) URI",
+            example = "http://tong.visitkorea.or.kr/cms/resource/71/181971_image2_1.jpg"
+    ) private @Getter URI image;
+    @Schema(
+            description = "대표 이미지(썸네일, 150*100) URI",
+            example = "http://tong.visitkorea.or.kr/cms/resource/71/181971_image3_1.jpg"
+    ) private @Getter URI thumbnailImage;
+    @Schema(
+            description = "콘텐츠 최초 등록일",
+            example = "2022-06-24 10:43:57.0"
+    ) private @Getter Date createdTime;
+    @Schema(
+            description = "콘텐츠 수정일",
+            example = "2022-06-24 10:43:57.0"
+    ) private @Getter Date modifiedTime;
+    @Schema(
+            description = "교과서 속 여행지 여부",
+            example = "false"
+    ) private @Getter boolean bookTour;
+    @Schema(
+            description = "전화번호",
+            example = "02-3700-3900"
+    ) private @Getter String tel;
 
     public static final class AttractionVOBuilder {
 
