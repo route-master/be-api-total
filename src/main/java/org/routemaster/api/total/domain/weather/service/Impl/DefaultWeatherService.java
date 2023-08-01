6c4e27dfae4c66fa9fe4c514719000044f67fb64
eunbin00 <eun61n@gmail.com>
2023-08-01 17:26:57 +0900

    #14 - Feat: Add Short Forecast services

--- src/main/java/org/routemaster/api/total/domain/weather/service/Impl/DefaultWeatherService.java
+++ src/main/java/org/routemaster/api/total/domain/weather/service/Impl/DefaultWeatherService.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.routemaster.api.total.domain.weather.data.ShortForecastWeather;
 import org.routemaster.api.total.domain.weather.data.VeryShortForecastWeather;
 import org.routemaster.api.total.domain.weather.data.VeryShortLiveWeather;
 import org.routemaster.api.total.domain.weather.service.WeatherService;
@@ -108,6 +109,51 @@ public class DefaultWeatherService implements WeatherService {
                     try {
                         JsonNode jsonNode = mapper.readTree(str);
                         VeryShortForecastWeather vo = VeryShortForecastWeather.builder()
+                                .buildFromJsonNode(jsonNode)
+                                .build();
+                        return vo;
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
+        return result;
+    }
+
+    @Override
+    public Mono<ShortForecastWeather> getShortForecastWeather(String baseDate, String baseTime, Double latitude, Double longitude) {
+        gpsTransfer.setLat(latitude);
+        gpsTransfer.setLng(longitude);
+        gpsTransfer.transfer(gpsTransfer);
+
+        final DefaultUriBuilderFactory factory = new DefaultUriBuilderFactory(WeatherAPI.baseUrl);
+        factory.setEncodingMode(DefaultUriBuilderFactory.EncodingMode.VALUES_ONLY);
+
+        WebClient webClient = WebClient.builder()
+                .uriBuilderFactory(factory)
+                .baseUrl(WeatherAPI.baseUrl)
+                .build();
+
+        Mono<ShortForecastWeather> result = webClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/getVilageFcst")
+                        .queryParam("serviceKey", WeatherAPI.encodingKey)
+                        .queryParam("numOfRows", NUM_OF_ROWS)
+                        .queryParam("pageNo", PAGE_NO)
+                        .queryParam("dataType", DATA_TYPE)
+                        .queryParam("base_date", baseDate)
+                        .queryParam("base_time", baseTime)
+                        .queryParam("nx", gpsTransfer.getNx())
+                        .queryParam("ny", gpsTransfer.getNy())
+                        .build()
+                )
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .bodyToMono(String.class)
+                .map(str -> {
+                    ObjectMapper objectMapper = new ObjectMapper();
+                    try {
+                        JsonNode jsonNode = mapper.readTree(str);
+                        ShortForecastWeather vo = ShortForecastWeather.builder()
                                 .buildFromJsonNode(jsonNode)
                                 .build();
                         return vo;
--- src/main/java/org/routemaster/api/total/domain/weather/service/WeatherService.java
+++ src/main/java/org/routemaster/api/total/domain/weather/service/WeatherService.java
@@ -1,5 +1,6 @@
 package org.routemaster.api.total.domain.weather.service;
 
+import org.routemaster.api.total.domain.weather.data.ShortForecastWeather;
 import org.routemaster.api.total.domain.weather.data.VeryShortForecastWeather;
 import org.routemaster.api.total.domain.weather.data.VeryShortLiveWeather;
 import reactor.core.publisher.Mono;
@@ -8,5 +9,6 @@ public interface WeatherService {
 
     Mono<VeryShortLiveWeather> getVeryShortLiveWeather(String baseDate, String baseTime, Double latitude, Double longitude);
     Mono<VeryShortForecastWeather> getVeryShortForecastWeather(String baseDate, String baseTime, Double latitude, Double longitude);
+    Mono<ShortForecastWeather> getShortForecastWeather(String baseDate, String baseTime, Double latitude, Double longitude);
 
 }
