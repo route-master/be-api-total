5db50998a6d5642f4792e06419f125dd619a8763
eunbin00 <eun61n@gmail.com>
2023-08-03 16:21:18 +0900

    #14 - Feat: Add WeatherResponseHeather rest docs

--- src/main/java/org/routemaster/api/total/domain/weather/data/WeatherResponseHeader.java
+++ src/main/java/org/routemaster/api/total/domain/weather/data/WeatherResponseHeader.java
@@ -1,5 +1,6 @@
 package org.routemaster.api.total.domain.weather.data;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -7,12 +8,30 @@ import lombok.*;
 @Builder
 @Getter
 @Setter
+@Schema(
+        description = "기상청 단기예보조회 API 응답 데이터 헤더"
+)
 public class WeatherResponseHeader {
 
-    private String resultCode;
-    private String resultMessage;
-    private Integer numOfRows;
-    private Integer pageNo;
-    private Integer totalCount;
+    @Schema(
+            description = "API 호출 결과의 상태 코드",
+            example = "00"
+    ) private String resultCode;
+    @Schema(
+            description = "API 호출 결과의 상태 메시지",
+            example = "NORMAL_SERVICE"
+    ) private String resultMessage;
+    @Schema(
+            description = "한 페이지 당 결과 수",
+            example = "10"
+    ) private Integer numOfRows;
+    @Schema(
+            description = "페이지 번호",
+            example = "1"
+    ) private Integer pageNo;
+    @Schema(
+            description = "전체 결과 수",
+            example = "100"
+    ) private Integer totalCount;
 
 }
