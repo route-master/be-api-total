d314f480c4706e90261d0371841ff227503200db
eunbin00 <eun61n@gmail.com>
2023-08-17 15:22:42 +0900

    #18 - Fix: Change gcp credential path to credential json

--- src/main/java/org/routemaster/api/total/infra/storage/GoogleCloudStorageConfig.java
+++ src/main/java/org/routemaster/api/total/infra/storage/GoogleCloudStorageConfig.java
@@ -4,26 +4,30 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageOptions;
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
+@Slf4j
 @Configuration
 @Component
 @Getter
 public class GoogleCloudStorageConfig {
 
-    public static String credentialsPath;
+    public static String credentialJson;
     public static String projectId;
     public static String bucketName;
 
-    @Value("${cloud.gcp.storage.credentials.location}")
-    public void setCredentialsPath(String credentialsPath) {
-        this.credentialsPath = credentialsPath;
+    @Value("${cloud.gcp.storage.credentials.json}")
+    public void setCredentialsPath(String credentialJson) {
+        this.credentialJson = credentialJson;
     }
 
     @Value("${cloud.gcp.storage.project-id}")
@@ -38,8 +42,8 @@ public class GoogleCloudStorageConfig {
 
     @Bean
     public Storage storage() throws IOException {
-        ClassPathResource resource = new ClassPathResource(credentialsPath);
-        GoogleCredentials credentials = GoogleCredentials.fromStream(resource.getInputStream());
+        InputStream is = new ByteArrayInputStream(credentialJson.getBytes());
+        GoogleCredentials credentials = GoogleCredentials.fromStream(is);
         return StorageOptions.newBuilder()
                 .setProjectId(projectId)
                 .setCredentials(credentials)
