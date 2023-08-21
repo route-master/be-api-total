ad89ddad6858a7e5b6980b42608e675ed6bc1d3e
eunbin00 <eun61n@gmail.com>
2023-08-21 18:00:06 +0900

    #6 - Feat: Add content-id based review search controller

---
+++ src/main/java/org/routemaster/api/total/domain/attraction/controller/AttractionReviewRestController.java
@@ -0,0 +1,26 @@
+package org.routemaster.api.total.domain.attraction.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.routemaster.api.total.domain.attraction.data.review.AttractionReview;
+import org.routemaster.api.total.domain.attraction.data.review.AttractionReviewSaveRequest;
+import org.routemaster.api.total.domain.attraction.service.AttractionReviewService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/attraction/review")
+@RequiredArgsConstructor
+public class AttractionReviewRestController {
+
+    private final AttractionReviewService service;
+
+    @GetMapping("/list/{attractionContentId}")
+    public ResponseEntity<Flux<AttractionReview>> attractionReviewList(@PathVariable(name = "attractionContentId") String attractionContentId) {
+        return new ResponseEntity<>(service.listByContentId(attractionContentId), HttpStatus.OK);
+    }
+}
